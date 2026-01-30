6b9fc070901f0f1bca9f2c868117fae4aa4e99cd
computare-viktoriun <flaviolds22@gmail.com>
2026-01-30 09:47:17 -0300

    chore: started django project

---
+++ web_loja/web_loja/urls.py
@@ -0,0 +1,37 @@
+"""
+URL configuration for web_loja project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('v1/', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('v1/', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('v1/blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+
+from rest_framework.routers import DefaultRouter
+from accounts.views import BaseUserViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+router = DefaultRouter()
+router.register(r'users', BaseUserViewSet)
+
+urlpatterns = [
+    path('v1/api-token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('v1/api-token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('v1/admin/', admin.site.urls),
+    path('v1/accounts/', include(router.urls))
+]
+
